19dd50dc35438af31f80f666f6bb401fd6d7411d
David Wursteisen <david.wursteisen+git@gmail.com>
2022-10-08 11:57:50 +0200

    Add test for configuration cache

--- developer-plugin/src/functionalTest/kotlin/com/github/minigdx/gradle/plugin/MinigdxKotlinMppPluginFunctionalTest.kt
+++ developer-plugin/src/functionalTest/kotlin/com/github/minigdx/gradle/plugin/MinigdxKotlinMppPluginFunctionalTest.kt
@@ -6,6 +6,7 @@ package com.github.minigdx.gradle.plugin
 import java.io.File
 import org.gradle.testkit.runner.GradleRunner
 import org.gradle.testkit.runner.TaskOutcome
+import org.junit.Ignore
 import org.junit.Rule
 import org.junit.rules.TemporaryFolder
 import kotlin.test.Test
@@ -32,14 +33,46 @@ class MinigdxKotlinMppPluginFunctionalTest {
         """)
 
         // Run the build
-        val runner = GradleRunner.create()
-        runner.forwardOutput()
-        runner.withPluginClasspath()
-        runner.withArguments("build")
-        runner.withProjectDir(projectDir)
-        val result = runner.build();
+        val result = GradleRunner.create()
+            .forwardOutput()
+            .withPluginClasspath()
+            .withArguments("build")
+            .withProjectDir(projectDir)
+            .build()
 
         // Verify the result
         assertEquals(TaskOutcome.SUCCESS, result.task(":build")?.outcome)
+    }
+
+    @Test
+    @Ignore("Configuration Cache not yet supported by Kotlin Multiplatform")
+    fun `can build with configuration cache`() {
+        // Setup the test build
+        val projectDir = temporaryFolder.newFolder("build", "functionalTest")
+        projectDir.mkdirs()
+        projectDir.resolve("settings.gradle").writeText("")
+        projectDir.resolve("build.gradle").writeText("""
+            plugins {
+                id('com.github.minigdx.gradle.plugin.developer.mpp')
+            }
+        """)
+
+        // Run the build
+        GradleRunner.create()
+            .forwardOutput()
+            .withPluginClasspath()
+            .withArguments("build", "--configuration-cache")
+            .withProjectDir(projectDir)
+            .build()
+
+        val result = GradleRunner.create()
+        .forwardOutput()
+        .withPluginClasspath()
+        .withArguments("build", "--configuration-cache")
+        .withProjectDir(projectDir)
+       .build()
+
+        // Verify the result
+        assertTrue(result.output.contains("Reusing configuration cache."))
     }
 }
